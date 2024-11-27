4f2b85b40cd0f9c38a24f8ff2e31616f267d2a94
webcoder42 <156161898+webcoder42@users.noreply.github.com>
2024-11-27 23:41:11 +0500

    Update UserLevelRoute.js

--- Routes/UserLevelRoute.js
+++ Routes/UserLevelRoute.js
@@ -1,16 +1,32 @@
 import express from "express";
 import {
+  //checkSalaryOnLevelUnlock,
+  //claimSalary,
+  getCurrentUserLevel,
   getLevelsByCurrency,
+  //getSalaryForCurrentLevel,
   unlockLevel,
 } from "../Controller/UserLevelController.js";
 import { requireSignIn } from "./../middleware/UserMiddleware.js";
 
 const router = express.Router();
 
-// Route for upgrading user level
-router.post("/upgrade-level", requireSignIn, unlockLevel);
+// Route for unlock user level
+router.post("/unlock-level", requireSignIn, unlockLevel);
 
 // get level
 router.get("/get-level", requireSignIn, getLevelsByCurrency);
+
+//get single
+router.get("/user-level", requireSignIn, getCurrentUserLevel);
+
+// Route to get salary for current level of user
+//router.get("/salary", requireSignIn, getSalaryForCurrentLevel);
+
+// Route to check and increment salary on level unlock
+//router.post("/salary-level-unlock", requireSignIn, checkSalaryOnLevelUnlock);
+
+// Route for salary claim
+//router.post("/salary/claim", requireSignIn, claimSalary);
 
 export default router;
