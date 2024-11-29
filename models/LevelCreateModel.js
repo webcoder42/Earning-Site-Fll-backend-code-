e7a3820ce723952aafe9eb5a2cb5fae043c655df
webcoder42 <156161898+webcoder42@users.noreply.github.com>
2024-11-29 08:33:31 +0500

    Update LevelCreateModel.js

--- models/LevelCreateModel.js
+++ models/LevelCreateModel.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const LevelCreationSchema = new mongoose.Schema(
   {
     Level: { type: String, required: true, default: 0 },
-    upgradeCommision: { type: String, required: true },
-    MonthlySalary: { type: String, required: true },
+    upgradeCommision: { type: Number, required: true },
+    MonthlySalary: { type: Number, required: true },
     MinAmount: { type: String, required: true },
     MinActiveRef: { type: String, required: true },
     currency: {
